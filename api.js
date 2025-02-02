e799f41fdef190dcdfced04b95b0c8386e445d50
Aman Singh <91814909+AmanS369@users.noreply.github.com>
2025-02-02 20:33:47 +0530

    Updated cors

--- api.js
+++ api.js
@@ -16,10 +16,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "https://app.shrl.live",
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
   }),
 );
 
